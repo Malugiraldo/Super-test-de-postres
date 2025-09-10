eea0baa03f4255eec4a94a56b8e5efa46c45a265
Malugiraldo <mgiraldo1206@gmail.com>
2025-09-10 22:03:42 +0000

    Cambios oraculo

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ const imagenes = [
   "https://www.redpathsugar.com/sites/redpathsugar_com/files/styles/m/public/Churros_Dulce_de_Leche_500x400.jpg.webp?itok=skVoJhyp",
   "https://i.notrefamille.com/1200x0/smart/2020/09/24/53061-large.jpg",
   "https://www.kingarthurbaking.com/sites/default/files/2022-12/KABC_Quick-Breads_Blueberry-Muffin_08304.jpg",
-  "https://www.ourhomemadeeasy.com/wp-content/uploads/2025/04/Birthday-Cake-Shake-Featured-Image-735x735.png",
+  "https://www.sugarandsoul.co/wp-content/uploads/2017/04/best-birthday-cake-milkshake-2.jpg",
+  "https://whatmollymade.com/wp-content/uploads/2017/03/chocolate-chip-pudding-cookies-recipe.jpg",
+  "https://www.reluctantgourmet.com/wp-content/uploads/2012/07/classic-tiramisu-r.jpg",
+  "https://media.tacdn.com/media/attractions-splice-spp-674x446/0f/b9/d1/18.jpg",
+  "https://www.simplyorganic.com/media/wysiwyg/tmp/simply-organic-recipe-vanilla-bean-apple-pie-900x900.jpg",
 ];
 
 const titulos = [
@@ -15,6 +19,11 @@ const titulos = [
   "Pain Au Chocolat",
   "Muffin de Arándanos",
   "Malteada de Cumpleaños",
+  "Galletas de Chispas de Chocolate",
+  "Tiramisu",
+  "Mochi",
+  "Pie de Manzana",
+
 ];
 
 const frases = [
@@ -24,6 +33,11 @@ const frases = [
   "La dualidad te acompaña: placer y disciplina, trabajo y descanso. Hoy deberás encontrar el equilibrio entre lo que deseas y lo que debes hacer. Consejo: un respiro en medio de la rutina te dará claridad.",
   "Los frutos de tu esfuerzo comienzan a notarse, aunque en pequeño. Hoy recibirás una señal de progreso o reconocimiento. Consejo: celebra lo pequeño, porque anuncia lo grande.",
   "Hoy tu día tiene un aire de renovación y celebración. Es buen momento para cerrar ciclos y abrir otros con alegría. Consejo: haz algo simbólico que te recuerde que cada día puedes empezar de nuevo.",
+  "Tu día estará marcado por la comodidad y el cobijo de lo conocido. Habrá un ambiente de cercanía que te recordará que no necesitas nada extraordinario para sentirte bien. Consejo: busca refugio en lo que te hace sentir en casa, incluso si estás lejos de ella.",
+  "Hoy será un día de capas: lo que ves en la superficie no es todo lo que ocurre debajo. Habrá profundidad en tus conversaciones y momentos de revelación. Consejo: tómate el tiempo de ir más allá de lo obvio.",
+  "Tu jornada tendrá un aire delicado pero poderoso. Algo sutil abrirá puertas que no imaginabas y traerá suavidad a lo difícil. Consejo: la ternura también puede ser una forma de fuerza",
+  "Tu día será un refugio de ternura y cercanía. Habrá un llamado a disfrutar de la calma del hogar, de los afectos que abrigan y de esos instantes sencillos que saben a familia. Consejo: permítete detenerte y abrigarte en lo que te hace sentir protegido.",
+
 ];
 
 // Trae los elementos del HTML que tienen ese id
